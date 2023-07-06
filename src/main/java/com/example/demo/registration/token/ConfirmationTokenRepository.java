0673ba902b0cb22d00d6ccdd58223e4fcbce3de4
syed <talentinfo9999@gmail.com>
2023-07-06 13:04:15 +0530

    removed query

--- src/main/java/com/example/demo/registration/token/ConfirmationTokenRepository.java
+++ src/main/java/com/example/demo/registration/token/ConfirmationTokenRepository.java
@@ -18,9 +18,6 @@ public interface ConfirmationTokenRepository
 
     @Transactional
     @Modifying
-    @Query("UPDATE ConfirmationToken c " +
-            "SET c.confirmedAt = ?2 " +
-            "WHERE c.token = ?1")
     int updateConfirmedAt(String token,
                           LocalDateTime confirmedAt);
 }
